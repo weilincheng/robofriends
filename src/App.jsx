08b39b5ad2fd1eaa6a21fd02a904d8658b57768e
weilincheng <7236196+weilincheng@users.noreply.github.com>
2021-04-08 23:20:29 +0800

    Add Scroll component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-import "./App.css";
+import Scroll from './Scroll';
+import './App.css';
 
 class App extends Component {
   constructor() {
@@ -14,31 +15,30 @@ class App extends Component {
 
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response => response.json())
-      .then(users => this.setState({robots: users}));
+      .then((response) => response.json())
+      .then((users) => this.setState({ robots: users }));
   }
-
 
   onSearchChange = (event) => {
-    this.setState({searchfield: event.target.value});
-  
+    this.setState({ searchfield: event.target.value });
   }
-
 
   render() {
-  const filteredRobots = this.state.robots.filter((robots) => robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase()));
-  if (this.state.robots.length === 0) {
-    return <h1>Loading</h1>
-  } else {
-    return (
-      <div className="tc">
-        <h1 className="f1">RoboFriends</h1>
-        <SearchBox searchChange={this.onSearchChange} />
-        <CardList robots={filteredRobots} />
-      </div>
-    );
+    const filteredRobots = this.state.robots.filter((robots) => robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase()));
+    if (this.state.robots.length === 0) {
+      return <h1>Loading</h1>;
+    } else {
+      return (
+        <div className="tc">
+          <h1 className="f1">RoboFriends</h1>
+          <SearchBox searchChange={this.onSearchChange} />
+          <Scroll>
+            <CardList robots={filteredRobots} />
+          </Scroll>
+        </div>
+      );
+    }
   }
-}
 }
 
 export default App;
---
+++ src/Scroll.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Scroll = (props) => (
+  <div style={{ overflow: 'scroll', border: '5px solid black', height: '800px' }}>
+    {props.children}
+  </div>
+);
+
+export default Scroll;
