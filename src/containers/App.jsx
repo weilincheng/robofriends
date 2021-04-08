7588fb10427c4e83b4ee9b2abde9c96c0d8a1190
weilincheng <7236196+weilincheng@users.noreply.github.com>
2021-04-08 23:41:58 +0800

    Reorganize files into containers and components dir

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import CardList from './CardList';
-import SearchBox from './SearchBox';
-import Scroll from './Scroll';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
 import './App.css';
 
 class App extends Component {
@@ -24,11 +24,11 @@ class App extends Component {
   }
 
   render() {
-    const filteredRobots = this.state.robots.filter((robots) => robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase()));
-    if (this.state.robots.length === 0) {
-      return <h1>Loading</h1>;
-    } else {
-      return (
+    const { robots, searchfield } = this.state;
+    const filteredRobots = robots.filter((robot) => robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase()));
+    return !robots.length ?
+      <h1>Loading</h1> :
+      (
         <div className="tc">
           <h1 className="f1">RoboFriends</h1>
           <SearchBox searchChange={this.onSearchChange} />
@@ -37,7 +37,6 @@ class App extends Component {
           </Scroll>
         </div>
       );
-    }
   }
 }
 
