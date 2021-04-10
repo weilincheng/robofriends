feb5d9eff6c9d4b44277fe4c3f348ffd9f86bf82
weilincheng <7236196+weilincheng@users.noreply.github.com>
2021-04-10 23:27:23 +0800

    Add ErrorBoundary and gh-pages

--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -4,15 +4,15 @@ import Card from './Card';
 const CardList = ({ robots }) => (
   <div>
     {
-                robots.map((user, i) => (
-                  <Card
-                    key={i}
-                    id={robots[i].id}
-                    name={robots[i].name}
-                    email={robots[i].email}
-                  />
-                ))
-            }
+      robots.map((user, i) => (
+        <Card
+          key={i}
+          id={robots[i].id}
+          name={robots[i].name}
+          email={robots[i].email}
+        />
+      ))
+    }
   </div>
 );
 
